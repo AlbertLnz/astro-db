da25f88ca3eb328287f60b9426eedf4ab73d2f84
AlbertLnz <albert2000_lanza@hotmail.com>
2024-06-14 12:45:37 +0200

    Tables created

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,49 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const User = defineTable({
+  columns: {
+    id: column.text({
+      primaryKey: true
+    }),
+    email: column.text({
+      unique: true
+    }),
+    username: column.text({
+      unique: true
+    })
+  }
+})
+
+const Todo = defineTable({
+  columns: {
+    id: column.text({
+      primaryKey: true
+    }),
+    title: column.text(),
+    description: column.text(),
+    user_id: column.text({
+      references: () => User.columns.id
+    }),
+    category_id: column.text({
+      references: () => Category.columns.id
+    })
+  }
+})
+
+const Category = defineTable({
+  columns: {
+    id: column.text({
+      primaryKey: true
+    }),
+    label: column.text({
+      unique: true
+    })
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    User, Todo, Category
+  }
 });
