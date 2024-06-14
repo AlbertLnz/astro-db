c3d91d96e96c03fca2fbad448bbfb6ee3e95060f
AlbertLnz <albert2000_lanza@hotmail.com>
2024-06-14 13:09:16 +0200

    Seed user data & show them

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,20 @@
-import { db } from 'astro:db';
+import { db, User } from 'astro:db';
+import { v4 as uuidv4 } from 'uuid'
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	
+	await db.insert(User).values([
+		{
+			id: uuidv4(),
+			email: "james_cameron@gmail.com",
+			username: "JCameron"
+		},
+		{
+			id: uuidv4(),
+			email: "steven_spielberg@outlook.com",
+			username: "SSpielberg"
+		},
+	])
+
 }
